26e6fd3db823e31d2655591401ad6c366d42f195
Adhar Singh <130926943+MciSA2023@users.noreply.github.com>
2025-08-06 18:55:54 +0200

    fix: update Footer layout for improved readability and structure

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,14 +9,15 @@ const Footer: React.FC = () => {
       <div className="absolute inset-0 bg-cover bg-center opacity-30 blur-sm z-0" style={{ backgroundImage: `url(${footerimage})` }} />
       <div className="relative z-10">
         <h2 className="font-bold text-4xl md:text-5xl">APVisuals</h2>
-        <div className="mt-4 flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-8">
-          <p className="text-gray-400 hover:text-amber-400">© 2025 APVisuals. All rights reserved.</p>
-          <a className='text-gray-400 hover:text-amber-400'> AGB </a>
-          <p>|</p>
-          <a className='text-gray-400 hover:text-amber-400'> Datenschutzerklärung </a>
-          <p>|</p>
-          <a className='text-gray-400 hover:text-amber-400'> Impressum </a>
-        </div>
+        <div className='text-gray-500 text-sm text-center mt-10 border-t border-gray-700 pt-6 w-screen'></div>
+          <div className="mt-4 flex flex-col md:flex-row items-center justify-center space-y-2 md:space-y-0 md:space-x-8">
+            <a className='text-gray-400 hover:text-amber-400'> AGB </a>
+            <p>|</p>
+            <a className='text-gray-400 hover:text-amber-400'> Datenschutzerklärung </a>
+            <p>|</p>
+            <a className='text-gray-400 hover:text-amber-400'> Impressum </a>
+          </div>
+            <p className="text-gray-400 hover:text-amber-400 items-center justify-center">© 2025 APVisuals. All rights reserved.</p>
       </div>
     </footer>
   );
